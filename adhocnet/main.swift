923a46f86e193bbc4889a9af01905ff86685e2ad
Joe Hildebrand <joe-github@cursive.net>
2015-11-14 21:21:30 -0700

    Initial Commit

---
+++ adhocnet/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  adhocnet
+//
+//  Created by Joe Hildebrand on 11/14/15.
+//  Copyright © 2015 Joe Hildebrand. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
