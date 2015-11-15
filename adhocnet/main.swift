78effda1bfe6d177e0981371ffe8a00f2bc39060
Joe Hildebrand <joe-github@cursive.net>
2015-11-15 00:00:52 -0700

    Working

--- adhocnet/main.swift
+++ adhocnet/main.swift
@@ -7,6 +7,33 @@
 //
 
 import Foundation
+import CoreWLAN
 
-print("Hello, World!")
+let args = Process.arguments
+if args.count < 3 {
+    let p = args[0].componentsSeparatedByString("/")
 
+    print("Usage: \(p.last!) <SSID> <Password>")
+    exit(64)
+}
+let networkName = args[1]
+let password = args[2]
+
+
+if let iface = CWWiFiClient.sharedWiFiClient().interface() {
+    do {
+        try iface.startIBSSModeWithSSID(
+            networkName.dataUsingEncoding(NSUTF8StringEncoding)!,
+            security: CWIBSSModeSecurity.WEP104,
+            channel: 11,
+            password: password as String
+        )
+        print("Success")
+    } catch let error as NSError {
+        print("Error", error)
+        exit(1)
+    }
+} else {
+    print("Invalid interface")
+    exit(1)
+}
